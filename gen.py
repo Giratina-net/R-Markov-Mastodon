5e3cd021dad7d5f47b4305ca03094ae582bef12e
ne53 <neko252222@gmail.com>
2023-09-05 01:20:02 +0900

    修正

--- gen.py
+++ gen.py
@@ -3,12 +3,10 @@ from os import getenv
 import json
 import requests
 
-api_base_url = getenv("API_BASE_URL"),
-client_id = getenv("API_CLIENT_ID"),
-client_secret = getenv("API_CLIENT_SECRET"),
-access_token = getenv("API_ACCESS_TOKEN")
-
-api = Mastodon(api_base_url = api_base_url,client_id = client_id,client_secret = client_secret,access_token = access_token)
+api = Mastodon( api_base_url = getenv('API_BASE_URL'),
+                            client_id = getenv('API_CLIENT_ID'),
+                            client_secret = getenv('API_CLIENT_SECRET'),
+                            access_token = getenv('API_ACCESS_TOKEN'))
 
 def generate_toot():
     tweet=json.loads(requests.get('https://api.giratina.net/v1/raika').text)['text']
